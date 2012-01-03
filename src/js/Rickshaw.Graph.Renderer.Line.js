f5026107242b1942fae05728e63d745c03c99004
Brad Anderson <brad@sankatygroup.com>
2012-01-03 14:58:58 -0500

    whitespace

--- src/js/Rickshaw.Graph.Renderer.Line.js
+++ src/js/Rickshaw.Graph.Renderer.Line.js
@@ -2,62 +2,62 @@ Rickshaw.namespace('Rickshaw.Graph.Renderer.Line');
 
 Rickshaw.Graph.Renderer.Line = function(args) {
 
-		var graph = this.graph = args.graph;
-		var self = this;
+    var graph = this.graph = args.graph;
+    var self = this;
 
-		this.name = 'line';
-		this.unstack = true;
+    this.name = 'line';
+    this.unstack = true;
 
-		graph.unstacker = graph.unstacker || new Rickshaw.Graph.Unstacker( { graph: graph } );
+    graph.unstacker = graph.unstacker || new Rickshaw.Graph.Unstacker( { graph: graph } );
 
-		this.seriesPathFactory = function() {
+    this.seriesPathFactory = function() {
 
-			return d3.svg.line()
-				.x( function(d) { return graph.x(d.x) } )
-				.y( function(d) { return graph.y(d.y) } )
-				.interpolate(this.graph.interpolation).tension(0.8);
-		}
+	return d3.svg.line()
+	    .x( function(d) { return graph.x(d.x) } )
+	    .y( function(d) { return graph.y(d.y) } )
+	    .interpolate(this.graph.interpolation).tension(0.8);
+    }
 
-		this.domain = function() {
+    this.domain = function() {
 
-			var values = [];
-			var stackedData = graph.stackedData || graph.stackData();
+	var values = [];
+	var stackedData = graph.stackedData || graph.stackData();
 
-			stackedData.forEach( function(series) {
-				series.forEach( function(d) {
-					values.push( d.y )
-				} );
-			} );
+	stackedData.forEach( function(series) {
+	    series.forEach( function(d) {
+		values.push( d.y )
+	    } );
+	} );
 
-			var xMin = stackedData[0][0].x;
-			var xMax = stackedData[0][ stackedData[0].length - 1 ].x;
+	var xMin = stackedData[0][0].x;
+	var xMax = stackedData[0][ stackedData[0].length - 1 ].x;
 
-			var yMin = 0;
-			var yMax = d3.max( values );
+	var yMin = 0;
+	var yMax = d3.max( values );
 
-			return { x: [xMin, xMax], y: [yMin, yMax] };
-		}
+	return { x: [xMin, xMax], y: [yMin, yMax] };
+    }
 
-		this.render = function() {
+    this.render = function() {
 
-			graph.vis.selectAll('*').remove();
+	graph.vis.selectAll('*').remove();
 
-			var nodes = this.graph.vis.selectAll("path")
-				.data(this.graph.stackedData)
-				.enter().append("svg:path")
-				.attr("d", this.seriesPathFactory());
+	var nodes = this.graph.vis.selectAll("path")
+	    .data(this.graph.stackedData)
+	    .enter().append("svg:path")
+	    .attr("d", this.seriesPathFactory());
 
-			var i = 0;
-			graph.series.forEach( function(series) {
-				if (series.disabled) return;
-				series.path = nodes[0][i++];
-				self._styleSeries(series);
-			} );
-		}
+	var i = 0;
+	graph.series.forEach( function(series) {
+	    if (series.disabled) return;
+	    series.path = nodes[0][i++];
+	    self._styleSeries(series);
+	} );
+    }
 
-		this._styleSeries = function(series) {
-			series.path.setAttribute('fill', 'none');
-			series.path.setAttribute('stroke', series.color);
-			series.path.setAttribute('stroke-width', 2);
-		}
+    this._styleSeries = function(series) {
+	series.path.setAttribute('fill', 'none');
+	series.path.setAttribute('stroke', series.color);
+	series.path.setAttribute('stroke-width', 2);
+    }
 }
